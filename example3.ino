37b31249a34cbddaade5f3a170c289a80ec8c3bd
Mohammed Sefath Chowdhury <mschowdh@buffalo.edu>
2018-11-28 06:58:56 -0500

    Add files via upload
    light up different styles using FastLED and microphone

---
+++ example3.ino
@@ -0,0 +1,45 @@
+
+/*****************  NEEDED TO MAKE NODEMCU WORK ***************************/
+#define FASTLED_INTERRUPT_RETRY_COUNT 0
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+/******************  LIBRARY SECTION *************************************/
+#include <FastLED.h>
+/*****************  LED LAYOUT AND SETUP *********************************/
+#define NUM_LEDS 10
+/*****************  DECLARATIONS  ****************************************/
+CRGB leds[NUM_LEDS];
+/*****************  GLOBAL VARIABLES  ************************************/
+const int ledPin = 6; 
+/*****************  SETUP FUNCTIONS  ****************************************/
+void setup() 
+{
+  Serial.begin(115200);
+  FastLED.addLeds<WS2812B, ledPin, RGB>(leds, NUM_LEDS);
+}
+/*****************  MAIN LOOP  ****************************************/
+/** i dont even know what this is, looks like melting rainbow icecream**/
+void loop() 
+{
+  leds[0] = CRGB(255, 0, 0);
+  FastLED.show();
+  delay(500);  
+  leds[1] = CRGB(0, 255, 0);
+  FastLED.show();
+  delay(500);
+  leds[2] = CRGB(0, 0, 255);
+  FastLED.show();
+  delay(500);
+  leds[3] = CRGB(150, 0, 255);
+  FastLED.show();
+  delay(500);
+  leds[4] = CRGB(255, 200, 20);
+  FastLED.show();
+  delay(500);
+  leds[5] = CRGB(85, 60, 180);
+  FastLED.show();
+  delay(500);
+  leds[6] = CRGB(50, 255, 20);
+  FastLED.show();
+  delay(500);
+  FastLED.show();
+}
